e9376e7ce75d66a4095120c3b99426bb4cc34c92
Matt McCabe <mattmadethis2@gmail.com>
2018-02-12 12:22:09 +0000

    Updated to system font stack and css variables

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,30 @@
-import { observable } from "aurelia-framework";
+import { inject } from 'aurelia-dependency-injection';
+import { observable, DOM } from "aurelia-framework";
 
 export class App {
-  public colours: string[] = [
-    'blackberry',
-    'strawberry',
-    'mango',
-    'lime',
-    'mint',
-    'white'
-  ];
+  public colours = ['blackberry', 'strawberry', 'mango', 'lime', 'mint', 'white'];
   public primaryColour = 'mint';
   public secondaryColour = 'lime';
   public backgroundColour = 'white';
   public flip: boolean = false;
   public leftEyebrow: boolean = false;
   public rightEyebrow: boolean = false;
-  public randomTimer: NodeJS.Timer;
-  public partyTimer: NodeJS.Timer;
+  @observable public goCrazy: boolean = false;
+  public goCrazyTimer: NodeJS.Timer;
   @observable public party: boolean = false;
-  @observable public scatter: boolean = false;
-
-  public scatterChanged(newValue) {
+  public partyTimer: NodeJS.Timer;
+  public goCrazyChanged(newValue) {
     if (newValue) {
       this.random();
     } else {
-      clearTimeout(this.randomTimer);
+      clearTimeout(this.goCrazyTimer);
       [].forEach.call(document.querySelectorAll('path, polyline, polygon'),
         (div) => setTransform(div, ''));
     }
   }
 
   public random() {
-    this.randomTimer = setInterval(() =>
+    this.goCrazyTimer = setInterval(() =>
       [].forEach.call(document.querySelectorAll('path, polyline, polygon'),
         (div) => randomTransform(div))
       , 400);
@@ -40,9 +33,9 @@ export class App {
   public partyChanged(newValue) {
     if (newValue) {
       this.partyTimer = setInterval(() => {
-        this.setPrimary(this.colours[getRandomInt(this.colours.length - 1)]);
-        this.setSecondary(this.colours[getRandomInt(this.colours.length - 1)]);
-        this.setBackground(this.colours[getRandomInt(this.colours.length - 1)]);
+        this.setPrimary(this.colours[getRandomInt(this.colours.length)]);
+        this.setSecondary(this.colours[getRandomInt(this.colours.length)]);
+        this.setBackground(this.colours[getRandomInt(this.colours.length)]);
       }
         , 200);
     } else {
@@ -53,14 +46,27 @@ export class App {
 
   public setPrimary(colour: string) {
     this.primaryColour = colour;
+    document.documentElement.style.setProperty('--avatar-primary', `var(--${colour})`);
   }
 
   public setSecondary(colour: string) {
     this.secondaryColour = colour;
+    document.documentElement.style.setProperty('--avatar-secondary', `var(--${colour})`);
   }
 
   public setBackground(colour: string) {
     this.backgroundColour = colour;
+    document.documentElement.style.setProperty('--avatar-background', `var(--${colour})`);
+  }
+
+  public reset() {
+    this.party = false;
+    this.leftEyebrow = false;
+    this.rightEyebrow = false;
+    this.flip = false;
+    this.goCrazy = false;
+    this.setPrimary('mint');
+    this.setSecondary('lime');
   }
 }
 
@@ -82,3 +88,4 @@ function setTransform(el, trans) {
   el.style.msTransform = trans;
   el.style.OTransform = trans;
 }
+.
